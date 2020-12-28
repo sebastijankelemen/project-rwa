bb32dc2b5a583f1cde734ac5ad95ce1d1dfe74ff
Sebastijan Kelemen <skelemen@racunarstvo.hr>
2020-12-28 03:10:03 +0100

    remove support for two-factor authentication

---
+++ Project.Rwa.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project.Rwa.Mvc.Startup))]
+namespace Project.Rwa.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
